b8e844cdf3d7ac90a6770a2e0fd053ab8068ba52
Bear <61859033+BubbaBean@users.noreply.github.com>
2026-02-03 14:35:20 -0800

    Heisentest Fix (bloodstream + damageable fail) (#3948)
    * maybe?
    
    * comments

--- Content.Shared/Body/Systems/SharedBloodstreamSystem.cs
+++ Content.Shared/Body/Systems/SharedBloodstreamSystem.cs
@@ -203,7 +203,8 @@ public abstract class SharedBloodstreamSystem : EntitySystem
 
         // TODO: Replace with RandomPredicted once the engine PR is merged
         // Use both the receiver and the damage causing entity for the seed so that we have different results for multiple attacks in the same tick
-        var seed = SharedRandomExtensions.HashCodeCombine((int)_timing.CurTick.Value, GetNetEntity(ent).Id, GetNetEntity(args.Origin)?.Id ?? 0 );
+        var originId = args.Origin != null && Exists(args.Origin.Value) ? GetNetEntity(args.Origin.Value).Id : 0; // IMP - Heisentest fix
+        var seed = SharedRandomExtensions.HashCodeCombine((int)_timing.CurTick.Value, GetNetEntity(ent).Id, originId); // IMP - Heisentest fix
         var rand = new System.Random(seed);
         var prob = Math.Clamp(totalFloat / 25, 0, 1);
         if (totalFloat > 0 && rand.Prob(prob))
